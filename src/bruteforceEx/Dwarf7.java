757a2608fe9cacbd3c6b0a1b000828dd1f5bdac8
woozink <k0109974@naver.com>
2022-06-12 14:34:09 +0900

    1

---
+++ src/bruteforceEx/Dwarf7.java
@@ -0,0 +1,26 @@
+package bruteforceEx;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Dwarf7 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void input() throws IOException{
+        int [] arr =new int[9];
+        for(int i = 0; i < 9; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        solution(arr);
+    }
+
+    public static void solution(int[] arr){
+        int sum = 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
